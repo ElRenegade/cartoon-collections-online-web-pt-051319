c71fc2540784ce79a434562387324c6ab58f55b5
Santiago Sanchez <chagostwo@yahoo.com>
2019-05-24 00:51:59 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,10 +9,8 @@ end
 def long_planeteer_calls(array)
   array.find{|word| if word.length > 4
     return true
-  elsif array.all?{|word| word.length >=4}
-
-
-  end}
+  elsif
+    return false
 end
 
 def find_the_cheese# code an argument here
